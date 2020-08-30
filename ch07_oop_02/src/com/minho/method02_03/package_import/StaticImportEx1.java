01bc7b263adc01587ddddc45e08c54fb9a547baa
ialsghi123 <ialsghi123@naver.com>
2020-08-31 00:48:27 +0900

    200831

--- ch07_oop_02/src/com/minho/method02_03/package_import/StaticImportEx1.java
+++ ch07_oop_02/src/com/minho/method02_03/package_import/StaticImportEx1.java
@@ -7,7 +7,7 @@ class StaticImportEx1 {
 	public static void main(String[] args) {	
 		// System.out.println(Math.random());
 		out.println(random());
- 
+  
 		// System.out.println("Math.PI :"+Math.PI);
 		out.println("Math.PI :" + PI);
 	}
