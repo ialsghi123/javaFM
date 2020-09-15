77eb2f1e608d35f6e3061e005244658be9580df0
ialsghi123 <ialsghi123@naver.com>
2020-09-15 23:38:20 +0900

    2020-09-15

--- Ch08_Exception_Handling/src/com/minho/exception_handling/NewExceptionTest.java
+++ Ch08_Exception_Handling/src/com/minho/exception_handling/NewExceptionTest.java
@@ -31,7 +31,7 @@ public class NewExceptionTest {
 		/* 파일들을 복사하는 코드를 적는다. */ }
 
 	static void deleteTempFiles() {
-		/* 임시파일들을 삭제하는 코드를 적는다. */}
+		/* 임시파일들을 삭제하는 코드를 적는다. */} 
 
 	static boolean enoughSpace() {
 		// 설치하는데 필요한 공간이 있는지 확인하는 코드를 적는다.
