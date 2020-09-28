09e476c93833c7c39b00cd52d48ff79f739eb66c
ialsghi123 <ialsghi123@naver.com>
2020-09-29 00:57:54 +0900

    2020-09-29

--- Ch10_Date,Time and Formatting/src/com/minho/formatting/DateFormatEx03.java
+++ Ch10_Date,Time and Formatting/src/com/minho/formatting/DateFormatEx03.java
@@ -2,6 +2,8 @@ package com.minho.formatting;
 
 import java.util.*;
 import java.text.*;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 class DateFormatEx03 {
 	public static void main(String[] args) {
@@ -12,5 +14,9 @@ class DateFormatEx03 {
 			Date d = df.parse("2015년 11월 23일");
 			System.out.println(df2.format(d));
 		} catch(Exception e) {}
+		
+		LocalDate date = LocalDate.of(2020, 5, 1);
+		System.out.println(date);
+		
 	} // main
 }
