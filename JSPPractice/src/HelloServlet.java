1da8e62b31ae0e8580ca7d605a5e05d4c4ca78ee
ialsghi123 <ialsghi123@naver.com>
2020-11-10 23:18:09 +0900

    2020-11-10

---
+++ JSPPractice/src/HelloServlet.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public HelloServlet() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		java.io.PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>Hello, Servlet!</h1>");
+		out.println("</body></html>");
+		out.close();
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		java.io.PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println("<html><body>");
+		out.println("<h1>Hello, " + name + "</h1>");
+		out.println("</body></html>");
+		out.close();
+	}
+
+}
---
+++ ServletPractice/WebContent/MyServlet.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>연습문제 4번</title>
+</head>
+<body>
+
+
+<form action="MyServlet" method="POST">
+<input type="radio" name="gender" value="남자">남자
+<input type="radio" name="gender" value="여자">여자<br>
+<input type="text" name="phone" size=20><br>
+<input type="submit" value="확인">
+</form>
+
+
+</body>
+</html>
